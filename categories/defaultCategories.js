f2554a38583830a49368a5629a70a894222d5137
Erin Kerr <erin.kerr17@gmail.com>
2025-10-09 17:06:04 -0600

    feat: add default DJ categories with associated images

---
+++ categories/defaultCategories.js
@@ -0,0 +1,23 @@
+// default categories
+
+// DJs
+
+// bands
+
+// MVP:
+// make database for people with name + image
+export const defaultOptions = [
+  {
+    name: "Martin Garrix",
+    images: ["images/martin_garrix.png"],
+    category: "dj",
+  },
+  { name: "David Guetta", images: ["images/david_guetta.png"], category: "dj" },
+  { name: "Alok", images: ["images/alok.png"], category: "dj" },
+  { name: "Fred Again...", images: ["images/fred_again.png"], category: "dj" },
+  {
+    name: "Timmy Trumpet",
+    images: ["images/timmy_trumpet.png"],
+    category: "dj",
+  },
+];
